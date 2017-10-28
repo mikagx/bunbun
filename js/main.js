9ba943fa62d6fc9ec981a2974d4ac041f3c0217d
Mika Gu <mikagu@Mikas-Air.wv.cc.cmu.edu>
2017-10-27 23:39:33 -0400

    commiting the website

--- js/main.js
+++ js/main.js
@@ -1,126 +1,74 @@
 var allTheBun = [];
 
-//Document ready, click add-to-cart, get value of the product
+//Document ready
 $(document).ready(function() {
 
+	//Change the price based on users' selection of size
+	$(function() { 
+    $('#dropdownSize').change(function() {
+         $('#textToSwap').html($(this.options[this.selectedIndex]).attr('price'));
+    }).change(); // Trigger the event
+	});
+
+	// Limit user's selection of flavor up to 3
+	var limit = 3;
+		$('input.single-checkbox').on('change', function(evt) {
+			console.log("input checkbox change registered");
+   		if($(this).siblings(':checked').length >= limit) {
+       this.checked = false;
+    }
+	});
+
+	//click add-to-cart, get value of the product, prepare for shopping cart
 	$('#add-to-cart').click(function(){
-    getDropdownValue();
+
+	var dropdownImageValue = $("#imageToSwap").attr("src");
+    var dropdownSizeValue = $("#dropdownSize option:selected").text();
+	var dropdownQuantityValue = $("#dropdownQuantity option:selected").text();
+	var dropdownPriceValue = $("#textToSwap").text();
+	var checkboxAllName = [];
+	$('#checkbox-section input:checked').each(function() {
+     	checkboxAllName.push($(this).attr('id'));
+	});
+
+    var item = {dropdownImageValue, dropdownSizeValue, dropdownQuantityValue, checkboxAllName, dropdownPriceValue};
+    var oldItems = JSON.parse(localStorage.getItem("allTheBun")) || [];
+
+    oldItems.push(item);
+	localStorage.setItem("allTheBun", JSON.stringify(oldItems));
+	console.log("This is selectedItem.size" + " " + oldItems);
+
+	console.log(oldItems[0]);
+	document.getElementById("cart-number").innerHTML = oldItems.length; 
     });
 
-    document.getElementById('cart-number').innerHTML = getCurrentCartValue();
+	//push the value of the product to shopping cart page
+	var newItems = JSON.parse(localStorage.getItem("allTheBun")) || [];
+	console.log(newItems);
 
-})
+		for (var i = 0; i < newItems.length; i++) {
 
+ 			var cartItem = $("<div class=\"bunsInTheCart\"><h3>Flavor: " + newItems[i]['checkboxAllName'] + "</h3>" 
+ 				+ "<img src="+ newItems[i]['dropdownImageValue']+" />"
+		    	+ "<h4>Size: " + newItems[i]['dropdownSizeValue'] + "</h4>" 
+		    	+ "<h4>Quantity: " + newItems[i]['dropdownQuantityValue'] + "</h4>" 
+		    	+ "<h4>" + newItems[i]['dropdownPriceValue'] + "</h4>"
+		    	+ "<button class='delete-item'><strong>remove<strong></button></div>");
 
-function getCurrentCartValue() {
+		    cartItem.data("item", newItems[i]);
+		    $("#shopping-cart").append(cartItem);
+		};
 
-	var val = localStorage.getItem("currentVal");
-	if(val) {
-		return val;
-	}
-	return 0;
-}
+		$('.delete-item').click(function(){
+	    	var parent = $(this).parent();
+	    	var item = parent.data("item")
+	    	parent.remove();
+	    	var index = newItems.indexOf(item)
+	    	newItems.splice(index, 1)
+	    	localStorage.setItem("allTheBun", JSON.stringify(newItems))
+	    	document.getElementById('cart-number').innerHTML = newItems.length;
+	    });
 
-function setCurrentCartValue() {
+    document.getElementById('cart-number').innerHTML = newItems.length;
 
-	var currentVal = getCurrentCartValue(); 
-	currentVal++; // instead of ++ +=6 
-	document.getElementById("cart-number").innerHTML = currentVal; 
-	localStorage.setItem("currentVal", currentVal);
-}
-
-function modifyCart() {
-
-	document.getElementById("#cart-number").innerHTML = 1;
-	localStorage.setItem('cartnumber', 1);
-}
-
-function getDropdownValue(){
-	 var dropdownSizeValue = $("#dropdownSize option:selected").text();
-	 var dropdownQuantityValue = $("#dropdownQuantity option:selected").text();
-	 var dropdownImgValue = $("")
-	 var checkboxAllName = [];
-	 $('#checkbox-section input:checked').each(function() {
-     	checkboxAllName.push($(this).attr('id'));
-	 });
-
-     var item = {size: dropdownSizeValue, quantity: dropdownQuantityValue, flavor: checkboxAllName};
-     // var oldItems = JSON.parse(localStorage.getItem("itemsArray")) || [];
-     allTheBun.push(item);
-
-     localStorage.setItem("itemsArray", JSON.stringify(allTheBun));
-
-     var newItems = localStorage.getItem("itemsArray");
-
-     var newItemsInfo = JSON.parse(newItems);
-
-     var shoppingCart = $("#shopping-cart");
-
-     var i = 0;
-     if (i < newItemsInfo.length) {
-     shoppingCart.append("<h5>" + console.log(newItemsInfo[i].flavor) + "</h5>" + "<h5>" + console.log(newItemsInfo[i].size) + "</h5>" + "<h5>" + 
-    	console.log(newItemsInfo[i].quantity) + "</h5>" + "<button class='delete-item'>remove</button>");
-    i++;
-    }
-
-    
-
-     $("#product-title-1").text(allTheBun.size + " the " + allTheBun.quantity);
-     console.log("This is selectedItem.size" + " " + JSON.stringify(allTheBun) + " ");
-};
-
-
-
-
-
-
-/*
-function save(){
-    var checkbox = document.getElementById('Bacon');
-    localStorage.setItem('Bacon', checkbox.checked);
-    console.log(localStorage.getItem('Bacon'));
-}
-*/
-
-// function updateItemName() {
-// 	var obj1 = JSON.parse(localStorage.getItem('itemsArray'));
-// 	console.log('This is the size of the product selected : ' + obj1[0]['checkboxAllName'][0]);
-// 	var itemName = obj1[0]['checkboxAllName'][0];
-// 	document.getElementById('#cart-item-name').innerHTML(itemName);
-// }
-
-
-/*
-function updateItemName() {
-	var obj = localStorage.getItem('itemsArray');
-	document.getElementById('cart-item-name').value = obj;
-    console.log(obj);
-	storage.setItem('cart-item-name', obj);
-}
-*/
-
-/*
-function updateItemName() {
-	var obj = JSON.parse(localStorage.getItem('itemsArray'));
-	console.log('aaa : '+obj[0]['checkboxAllName'][0]);
-	var itemName = obj[0]['checkboxAllName'][0];
-	document.getElementById('cart-item-name').innerHTML = itemName;
-}
-*/
-
-     
-     
-     /*JSON.parse(selectedItem);
-
-     var theSpecificProduct = JSON.parse(localStorage.getItem("someitem"));
-     console.log(selectedItem);*/
-/*
-function updateCount(){
-	var storedCount = 0;
-	if 
-         
-		 $("#animal-name").text(animal.name +" the "+ animal.type);
-		 // update the image based on the animal’s image property  
-		 $("#animal-img").attr("src", animal.image);
-}
-*/
+});
